aa9c54c3b6809c1c629479976cff78e47ff83397
Tabish <1631995@gmail.com>
2024-02-15 17:22:19 +0100

    ssr fix

--- server.js
+++ server.js
@@ -58,11 +58,34 @@ app.post('/transcribe', upload.single('file'), async (req, res) => {
 });
 
 // Serve HTML
-app.use('/', async (req, res) => {
-    let url = req.originalUrl
-    let template = await fs.readFile('./index.html', 'utf-8')
-    template = await vite.transformIndexHtml(url, template)
-    res.status(200).set({'Content-Type': 'text/html'}).send(template)
+app.use('*', async (req, res) => {
+    try {
+        const url = req.originalUrl.replace(base, '')
+
+        let template
+        let render
+        if (!isProduction) {
+            // Always read fresh template in development
+            template = await fs.readFile('./index.html', 'utf-8')
+            template = await vite.transformIndexHtml(url, template)
+            render = (await vite.ssrLoadModule('/src/entry-server.js')).render
+        } else {
+            template = templateHtml
+            render = (await import('./dist/server/entry-server.js')).render
+        }
+
+        const rendered = await render(url, ssrManifest)
+
+        const html = template
+            .replace(`<!--app-head-->`, rendered.head ?? '')
+            .replace(`<!--app-html-->`, rendered.html ?? '')
+
+        res.status(200).set({ 'Content-Type': 'text/html' }).send(html)
+    } catch (e) {
+        vite?.ssrFixStacktrace(e)
+        console.log(e.stack)
+        res.status(500).end(e.stack)
+    }
 })
 
 // Start http server
